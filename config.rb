f3f0915c5bdaa9d24c3550dde5afc5850625f0bc
Manuel Meurer <manuel@krautcomputing.com>
2017-02-12 19:06:42 +0100

    dont sync automatically after build

--- config.rb
+++ config.rb
@@ -19,7 +19,6 @@ activate :s3_sync do |config|
   config.aws_secret_access_key = ENV.fetch('AWS_SECRET_ACCESS_KEY')
   config.bucket                = 'uplink.tech'
   config.region                = 'eu-central-1'
-  config.after_build           = false
 end
 
 activate :cdn do |cdn|
