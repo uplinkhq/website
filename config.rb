8592c24f470fb1fb275652eef6483c643835700d
Manuel Meurer <manuel@krautcomputing.com>
2017-12-04 16:00:03 +0100

    load blog posts from data file

--- config.rb
+++ config.rb
@@ -155,6 +155,29 @@ helpers do
   def markdown(text)
     Kramdown::Document.new(text).to_html
   end
+
+  def in_groups_of(data, number, fill_with = nil)
+    if number.to_i <= 0
+      raise ArgumentError,
+        "Group size must be a positive integer, was #{number.inspect}"
+    end
+
+    if fill_with == false
+      collection = data
+    else
+      # data.size % number gives how many extra we have;
+      # subtracting from number gives how many to add;
+      # modulo number ensures we don't add group of just fill.
+      padding = (number - data.size % number) % number
+      collection = data.dup.concat(Array.new(padding, fill_with))
+    end
+
+    if block_given?
+      collection.each_slice(number) { |slice| yield(slice) }
+    else
+      collection.each_slice(number).to_a
+    end
+  end
 end
 
 configure :development do
