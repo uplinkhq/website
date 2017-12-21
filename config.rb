0ff4ad8cf8b2155f06fbfbc48a1d535145ac342d
Manuel Meurer <manuel@krautcomputing.com>
2017-12-21 22:06:13 +0100

    add new deutsche startups blogpost

--- config.rb
+++ config.rb
@@ -88,24 +88,34 @@ set :tech_images, {
   'Java'       => 'java.png',
   'Spring'     => 'spring.png'
 }
-set :media_coverage, {
-  berlin_valley: {
-    link: 'https://drive.google.com/file/d/0B4gIb1U545pcZzRwT3FqemhRYVU/view',
-    text: 'Uplink ist ein Netzwerk für professionelle Freelance-Entwickler und hilft Berliner Unternehmen, die besten Entwickler der Stadt zu finden.'
-  },
-  deutsche_startups: {
-    link: 'https://www.deutsche-startups.de/2016/10/26/arbeiten-startups-erfolgreich-mit-freelancer-zusammen/',
-    text: 'So arbeiten Startups erfolgreich mit Freelancern zusammen'
-  },
-  debitoor: {
-    link: 'https://debitoor.de/blog/debitoor-community-manuel-meurer',
-    text: '2015 hast du mit Uplink eine Plattform für Freelance-Entwickler gegründet – und damit eine echte Nische besetzt.'
-  },
-  startup_valley_news: {
-    link: 'https://www.startupvalley.news/de/uplink-netzwerk-freelancer-it-recruitern/',
-    text: 'Macht eine Liste eurer Mitbewerber und schreibt sie alle nacheinander an!'
+set :media_coverage, [
+  {
+    outlet: :berlin_valley,
+    link:   'https://drive.google.com/file/d/0B4gIb1U545pcZzRwT3FqemhRYVU/view',
+    text:   'Uplink ist ein Netzwerk für professionelle Freelance-Entwickler und hilft Berliner Unternehmen, die besten Entwickler der Stadt zu finden.',
+    date:   Date.new(2016, 10, 11)
+  }, {
+    outlet: :deutsche_startups,
+    link:   'https://www.deutsche-startups.de/2016/10/26/arbeiten-startups-erfolgreich-mit-freelancer-zusammen/',
+    text:   'So arbeiten Startups erfolgreich mit Freelancern zusammen',
+    date:   Date.new(2016, 10, 26)
+  }, {
+    outlet: :deutsche_startups,
+    link:   'https://www.deutsche-startups.de/2017/12/20/entwickler-sollten-sich-selbststaendig-machen/',
+    text:   'Entwickler sollten sich selbstständig machen',
+    date:   Date.new(2017, 12, 20)
+  }, {
+    outlet: :debitoor,
+    link:   'https://debitoor.de/blog/debitoor-community-manuel-meurer',
+    text:   '2015 hast du mit Uplink eine Plattform für Freelance-Entwickler gegründet – und damit eine echte Nische besetzt.',
+    date:   Date.new(2017, 3, 7)
+  }, {
+    outlet: :startup_valley_news,
+    link:   'https://www.startupvalley.news/de/uplink-netzwerk-freelancer-it-recruitern/',
+    text:   'Macht eine Liste eurer Mitbewerber und schreibt sie alle nacheinander an!',
+    date:   Date.new(2017, 9, 29)
   }
-}
+]
 
 helpers do
   # TODO: path localization should work with the `url_for` helper.
